79e6dcc785a52cdb447e553d6fc9c749a944425e
Prathamesh <pratham.dahakepsd@gmail.com>
2025-06-05 17:54:21 +0530

    final commit

--- app.js
+++ app.js
@@ -2,14 +2,26 @@ let userScore = 0;
 let compScore = 0;
 let userChoice = document.querySelectorAll(".item");
 let compChoices = ["rock", "paper", "scissor"];
+let userPoint = document.querySelector("#you");
+let compPoint = document.querySelector("#comp");
+
 let compChoice;
+let note = document.querySelector("#note");
 
 let showWinner = (userWin) =>{
     if(userWin){
         console.log("You won!")
+        note.innerText = "Congratulations! You won";
+        note.style.backgroundColor = "Green";
+        userScore++;
+        you.innerText = userScore;
     }
     else{
         console.log("You Lost")
+        note.innerText = "Bad Luck! You lose";
+        note.style.backgroundColor = "Red";
+        compScore++;
+        comp.innerText = compScore;
     }
 }
 
@@ -22,7 +34,7 @@ let action = userChoice.forEach((item)=>{
         let userWin = true;
         if( item.id=== compChoice){
             console.log("its a draw");
-            return;
+            note.innerText = "It's a Draw!";            return;
         }
         else if(
             userChoice === "rock"
