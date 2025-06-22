6e6ea3f84bc514a90c4a57483508a156a9af2ea6
Prathamesh <pratham.dahakepsd@gmail.com>
2025-06-22 13:20:00 +0530

    Final code

--- app.js
+++ app.js
@@ -8,48 +8,38 @@ let compPoint = document.querySelector("#comp");
 let compChoice;
 let note = document.querySelector("#note");
 
-let showWinner = (userWin) =>{
-    if(userWin){
-        console.log("You won!")
-        note.innerText = "Congratulations! You won";
-        note.style.backgroundColor = "Green";
-        userScore++;
-        you.innerText = userScore;
-    }
-    else{
-        console.log("You Lost")
-        note.innerText = "Bad Luck! You lose";
-        note.style.backgroundColor = "Red";
-        compScore++;
-        comp.innerText = compScore;
-    }
-}
+let showWinner = (userWin) => {
+  if (userWin) {
+    console.log("You won!");
+    note.innerText = "Congratulations! You won";
+    note.style.backgroundColor = "Green";
+    userScore++;
+    you.innerText = userScore;
+  } else {
+    console.log("You Lost");
+    note.innerText = "Bad Luck! You lose";
+    note.style.backgroundColor = "Red";
+    compScore++;
+    comp.innerText = compScore;
+  }
+};
 
-let action = userChoice.forEach((item)=>{
-    item.addEventListener("click", ()=>{
-        console.log("User Choice= ", item.id )
-        let compIdx = Math.floor(Math.random()*3);
-        compChoice = compChoices[compIdx];
-        console.log("Computer Choice= ",compChoice);
-        let userWin = true;
-        if( item.id=== compChoice){
-            console.log("its a draw");
-            note.innerText = "It's a Draw!";            return;
-        }
-        else if(
-            userChoice === "rock"
-        ){
-            
-            userWin = compChoice === "paper" ? false : true;
-        }
-        else if (userChoice === "paper"){
-            userWin = compChoice ==="rock" ? true : false;
-        }
-        else{
-            userWin = compChoice === "rock"? false: true;
-        }
-         showWinner(userWin);
-    })
-   
-})
-
+let action = userChoice.forEach((item) => {
+  item.addEventListener("click", () => {
+    console.log("User Choice= ", item.id);
+    let compIdx = Math.floor(Math.random() * 3);
+    compChoice = compChoices[compIdx];
+    console.log("Computer Choice= ", compChoice);
+    let userWin = true;
+    if (
+      (item.id === "rock" && compChoice === "scissor") ||
+      (item.id === "paper" && compChoice === "rock") ||
+      (item.id === "scissor" && compChoice === "paper")
+    ) {
+      userWin = true;
+    } else {
+      userWin = false;
+    }
+    showWinner(userWin);
+  });
+});
